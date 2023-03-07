665decf5999e32a30a94496f644586836a02f0e8
Gursimran Singh <2042387@csl-student.wlv.ac.uk>
2023-03-07 18:16:06 +0000

    Load F1 Events and circuits to Db closes #19 closes #24

--- app/Models/F1EventModel.php
+++ app/Models/F1EventModel.php
@@ -9,22 +9,25 @@
         protected $table = 'ci_f1events';
         protected $allowedFields = ['id', 'circuitId', 'season','type', 'laps', 'date', 'status'];
 
+        //Set F1 Event From the database
         public function setEvent($event)
         {
             $this->upsert($event);
         }
 
-
+        //Get F1 Race From the database
         public function getEventById($id){
             return $this->where('id', $id)->first();
             
         }
 
+        //Get F1 Event of requested type From the database
         public function getEventByType($type){
             return $this->where('type', $type)->findAll();
             
         }
 
+        //Get next scheduled F1 Event from the database
         public function getNextEvent(){
             return $this->where('status', 'scheduled')->first();
             
